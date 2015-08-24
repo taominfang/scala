edfe9ad2ebde1eccfc3458a4dfcf4d2f6a44e7cc
Minfang <tmfjob@gmail.com>
2015-08-23 20:18:44 -0700

    update tools

--- SynchronizeFile/src/main/scala-2.11/org/openfly/tools/scala/syschronzie/SynchronizeChanged2Ftp.scala
+++ SynchronizeFile/src/main/scala-2.11/org/openfly/tools/scala/syschronzie/SynchronizeChanged2Ftp.scala
@@ -13,7 +13,7 @@ import scala.collection.mutable
  * Created by mtao60 on 8/18/15.
  */
 object SynchronizeChanged2Ftp {
-  def main(argv:Array[String]): Unit = {
+  def main(argv: Array[String]): Unit = {
     val p = ParameterParser(argv);
     p.add(Parameter("files-status").addAliasKey("-F").setRequired(true).setFollowingValueSize(1));
     p.add(Parameter("source-folder").addAliasKey("-s").setRequired(true).setFollowingValueSize(1))
@@ -23,6 +23,7 @@ object SynchronizeChanged2Ftp {
     p.add(Parameter("ftp-folder").addAliasKey("-t").setRequired(true).setFollowingValueSize(1))
     p.add(Parameter("force-copy-all").addAliasKey("-f").setRequired(false).setFollowingValueSize(0));
     p.add(Parameter("ftp-passive").addAliasKey("-fp").setRequired(false).setFollowingValueSize(0));
+    p.add(Parameter("dry-run").setRequired(false).setDesc("Only list the copy which need copy, do not do the copy action!"))
 
     p.add(Parameter("ignore-file-name").addAliasKey("-i").setRequired(false).setFollowingValueSize(1));
     p.add(Parameter("ignore-file-list-file").addAliasKey("-ifl").setRequired(false).setFollowingValueSize(1));
@@ -30,64 +31,72 @@ object SynchronizeChanged2Ftp {
 
     p.parse();
 
-    if(p.isSet("help")){
-      println ("Usage:"+p.usage)
+    if (p.isSet("help")) {
+      println("Usage:" + p.usage)
       return
     }
 
-    val es=p.getErrorMessage()
+    val es = p.getErrorMessage()
 
-    if(es.size>0){
+    if (es.size > 0) {
       es.foreach(println(_))
       println("error, quite")
-      println ("Usage:"+p.usage)
+      println("Usage:" + p.usage)
       return
     }
 
-    val sourceFolder=new File(p.getFirstValue("source-folder").get)
+    val sourceFolder = new File(p.getFirstValue("source-folder").get)
 
-    require(sourceFolder.isDirectory,sourceFolder.getPath+" is not folder");
+    require(sourceFolder.isDirectory, sourceFolder.getPath + " is not folder");
 
-    val skipNames=collection.mutable.Set[String]()
-    try{
-      val br=new BufferedReader(new FileReader(p.getFirstValue("ignore-file-list-file").get))
-      var line=br.readLine()
-      while(line!=null){
+    val skipNames = collection.mutable.Set[String]()
+    try {
+      val br = new BufferedReader(new FileReader(p.getFirstValue("ignore-file-list-file").get))
+      var line = br.readLine()
+      while (line != null) {
         skipNames.add(line)
-        line=br.readLine()
+        line = br.readLine()
       }
       br.close();
     }
-    catch{
-      case _ =>{}
+    catch {
+      case _ => {}
     }
 
-    val updateFileList=checkUpdate(sourceFolder.getPath,p.getFirstValue("files-status").get,skipNames,p.isSet("force-copy-all"))
+    val updateFileList = checkUpdate(sourceFolder.getPath, p.getFirstValue("files-status").get, skipNames, p.isSet("force-copy-all"))
 
-    if(updateFileList.size>0){
-      connectFtp(p.getFirstValue("ftp-server").get,p.getFirstValue("ftp-user").get,p.getFirstValue("ftp-password").get,p.isSet("ftp-passive"))
-      uploadFiles(updateFileList,p.getFirstValue("ftp-folder").get,sourceFolder.getPath)
-      disconnectFtp()
-      println("upload file finished, total file uploaded:"+updateFileList.size)
+    if (updateFileList.size > 0) {
+      if (!p.isSet("dry-run")) {
+        connectFtp(p.getFirstValue("ftp-server").get, p.getFirstValue("ftp-user").get, p.getFirstValue("ftp-password").get, p.isSet("ftp-passive"))
+        uploadFiles(updateFileList, p.getFirstValue("ftp-folder").get, sourceFolder.getPath)
+        disconnectFtp()
+        println("upload file finished, total file uploaded:" + updateFileList.size)
+      }
+      else{
+        println("dry-run...., file list")
+        updateFileList.foreach(println(_))
+      }
     }
-    else{
+    else {
       println("do not find any file need upload!")
     }
     //connect ftp
 
 
   }
-  val ftpConnect=new org.apache.commons.net.ftp.FTPClient
-  def connectFtp(host:String,user:String,password:String,ftp_passive:Boolean) ={
+
+  val ftpConnect = new org.apache.commons.net.ftp.FTPClient
+
+  def connectFtp(host: String, user: String, password: String, ftp_passive: Boolean) = {
     ftpConnect.connect(host)
-    require(ftpConnect.login(user,password),"can not login to ftp")
+    require(ftpConnect.login(user, password), "can not login to ftp")
 
     ftpConnect.setFileType(org.apache.commons.net.ftp.FTP.BINARY_FILE_TYPE)
 
-    if(ftp_passive){
+    if (ftp_passive) {
       ftpConnect.enterLocalPassiveMode()
     }
-    else{
+    else {
       ftpConnect.enterLocalActiveMode()
     }
 
@@ -98,54 +107,54 @@ object SynchronizeChanged2Ftp {
     true
   }
 
-  def disconnectFtp(): Unit ={
+  def disconnectFtp(): Unit = {
     ftpConnect.logout();
   }
 
-  def checkUpdate(root:String,status:String,skipFiles:collection.mutable.Set[String],force:Boolean)={
-    val allList=new mutable.MutableList[File]()
+  def checkUpdate(root: String, status: String, skipFiles: collection.mutable.Set[String], force: Boolean) = {
+    val allList = new mutable.MutableList[File]()
 
 
-    val r=new File(root);
-    require(r.isDirectory,r.getPath+" is not a folder");
-    FindAllFiles.findFiles(r,allList,skipFiles)
-    val statusMap=collection.mutable.Map[String,Long]();
-    try{
-      val statusFile=new File(status)
-      if(statusFile.isFile){
-        val t3=new FileReader(statusFile)
-        val t1=new JsonParser().parse(t3).getAsJsonObject.entrySet().iterator()
+    val r = new File(root);
+    require(r.isDirectory, r.getPath + " is not a folder");
+    FindAllFiles.findFiles(r, allList, skipFiles)
+    val statusMap = collection.mutable.Map[String, Long]();
+    try {
+      val statusFile = new File(status)
+      if (statusFile.isFile) {
+        val t3 = new FileReader(statusFile)
+        val t1 = new JsonParser().parse(t3).getAsJsonObject.entrySet().iterator()
 
-        while(t1.hasNext){
-          val t2=t1.next();
-          statusMap.update(t2.getKey,t2.getValue.getAsLong)
+        while (t1.hasNext) {
+          val t2 = t1.next();
+          statusMap.update(t2.getKey, t2.getValue.getAsLong)
         }
         t3.close()
 
       }
     }
     catch {
-      case _ =>{}
+      case _ => {}
     }
 
-    val rootLen=if(root.charAt(root.length-1)=='/')root.length else root.length+1
-    val updateFiles=mutable.MutableList[String]();
-    allList.foreach{
-      file=>{
-        val name=file.getPath.substring(rootLen);
-        if(! statusMap.contains(name) || statusMap(name)!=file.lastModified() || force){
-          updateFiles+=name
+    val rootLen = if (root.charAt(root.length - 1) == '/') root.length else root.length + 1
+    val updateFiles = mutable.MutableList[String]();
+    allList.foreach {
+      file => {
+        val name = file.getPath.substring(rootLen);
+        if (!statusMap.contains(name) || statusMap(name) != file.lastModified() || force) {
+          updateFiles += name
         }
-        statusMap.update(name,file.lastModified())
+        statusMap.update(name, file.lastModified())
       }
     }
 
     //wrtie back to status file
-    val jStatus=new JsonObject
-    statusMap.foreach(st=>{
-      jStatus.addProperty(st._1,st._2)
+    val jStatus = new JsonObject
+    statusMap.foreach(st => {
+      jStatus.addProperty(st._1, st._2)
     })
-    val fw=new FileWriter(status)
+    val fw = new FileWriter(status)
     fw.write(jStatus.toString)
     fw.close()
 
@@ -157,17 +166,16 @@ object SynchronizeChanged2Ftp {
   }
 
 
+  def confirmDir(dir: String): Unit = {
+    require(dir.charAt(0) == '/', "we need abstract path! it should start /, but we got:" + dir)
+    val paths = dir.split("/").toList
 
-  def confirmDir(dir:String): Unit ={
-    require(dir.charAt(0)=='/',"we need abstract path! it should start /, but we got:"+dir)
-    val paths=dir.split("/").toList
+    var p = "";
 
-    var p="";
-
-    paths.foreach{
-      a=> {
+    paths.foreach {
+      a => {
         if (a != "") {
-          p = p +"/"+ a;
+          p = p + "/" + a;
           if (!ftpConnect.changeWorkingDirectory(p)) {
             ftpConnect.makeDirectory(p)
             require(ftpConnect.changeWorkingDirectory(p), "Could not create the dir:" + p)
@@ -182,133 +190,129 @@ object SynchronizeChanged2Ftp {
 
   }
 
-  def uploadFiles(fileList:mutable.MutableList[String],_ftpRoot:String,_localRoot:String): Unit ={
+  def uploadFiles(fileList: mutable.MutableList[String], _ftpRoot: String, _localRoot: String): Unit = {
 
-     //currentDirs.foreach(println(_))
+    //currentDirs.foreach(println(_))
 
-    val removeLastSlish={(f:String)=>if(f.charAt(f.length-1)=='/') f.substring(0,f.length-2) else f}
+    val removeLastSlish = { (f: String) => if (f.charAt(f.length - 1) == '/') f.substring(0, f.length - 2) else f }
 
-    val ftpRoot=removeLastSlish(_ftpRoot)
-    val localRoot=removeLastSlish(_localRoot);
+    val ftpRoot = removeLastSlish(_ftpRoot)
+    val localRoot = removeLastSlish(_localRoot);
 
-    fileList.foreach{
-      name=>{
-        val ftpFilePath=ftpRoot+"/"+name
+    fileList.foreach {
+      name => {
+        val ftpFilePath = ftpRoot + "/" + name
 
-        val (dir, fn)=parseFullPath2DirAndFileName(ftpFilePath)
+        val (dir, fn) = parseFullPath2DirAndFileName(ftpFilePath)
         confirmDir(dir);
-        val localFilePath=localRoot+"/"+name
-        val input=new FileInputStream(localFilePath)
-        ftpConnect.storeFile(fn,input)
-        println("uploaded:"+localFilePath+" -> "+ftpFilePath);
+        val localFilePath = localRoot + "/" + name
+        val input = new FileInputStream(localFilePath)
+        ftpConnect.storeFile(fn, input)
+        println("uploaded:" + localFilePath + " -> " + ftpFilePath);
 
       }
     }
 
 
-
-
   }
 
-  def parseFullPath2DirAndFileName(path:String)={
-    val lastSlish=path.lastIndexOf('/');
+  def parseFullPath2DirAndFileName(path: String) = {
+    val lastSlish = path.lastIndexOf('/');
 
 
-    if(lastSlish == -1){
-      ("",path)
+    if (lastSlish == -1) {
+      ("", path)
     }
     else {
-      (path.substring(0,lastSlish),path.substring(lastSlish+1))
+      (path.substring(0, lastSlish), path.substring(lastSlish + 1))
     }
   }
 
-  def uploadFile(ftpFullPath:String,input:InputStream): Unit ={
+  def uploadFile(ftpFullPath: String, input: InputStream): Unit = {
 
-    val lastSlish=ftpFullPath.lastIndexOf('/');
+    val lastSlish = ftpFullPath.lastIndexOf('/');
 
 
-    if(lastSlish == -1){
-      println("uploading "+ftpFullPath)
-      ftpConnect.storeFile(ftpFullPath,input)
+    if (lastSlish == -1) {
+      println("uploading " + ftpFullPath)
+      ftpConnect.storeFile(ftpFullPath, input)
     }
     else {
-      val path=ftpFullPath.substring(0,lastSlish);
-      val fileName=ftpFullPath.substring(lastSlish+1);
+      val path = ftpFullPath.substring(0, lastSlish);
+      val fileName = ftpFullPath.substring(lastSlish + 1);
       ftpConnect.changeWorkingDirectory(path);
-      println("Change dir to:"+path)
+      println("Change dir to:" + path)
 
-      ftpConnect.storeFile(fileName,input)
-      println("Done uploading for "+fileName)
+      ftpConnect.storeFile(fileName, input)
+      println("Done uploading for " + fileName)
     }
 
 
   }
-
-
-
 
 
 }
 
-object FileSet{
-  val root=new FileSet()
+object FileSet {
+  val root = new FileSet()
 
-  def add(path:String): Unit ={
+  def add(path: String): Unit = {
 
-    val paths=if (path.charAt(0)=='/') path.substring(1).split("/") else path.split("/")
+    val paths = if (path.charAt(0) == '/') path.substring(1).split("/") else path.split("/")
 
-    root.add("","",path,paths,0)
+    root.add("", "", path, paths, 0)
 
   }
 
-  def getRoot={
+  def getRoot = {
     root
   }
 
-  def navigate(one:FileSet,space:String): Unit ={
-    println(space+"folder:"+one.my_name)
-    println(space+"folder full path:"+one.my_full_path)
-    val newSpace=space+"  ";
-    one.fileNames.foreach{fn=>println(newSpace+"File:"+fn._2)}
-    one.subFolders.foreach{
-      p=>{
+  def navigate(one: FileSet, space: String): Unit = {
+    println(space + "folder:" + one.my_name)
+    println(space + "folder full path:" + one.my_full_path)
+    val newSpace = space + "  ";
+    one.fileNames.foreach { fn => println(newSpace + "File:" + fn._2) }
+    one.subFolders.foreach {
+      p => {
 
-        navigate(p._2,newSpace)
+        navigate(p._2, newSpace)
       }
     }
     println(space)
   }
 }
 
-class FileSet(){
+class FileSet() {
 
 
   import collection.mutable._
-  val subFolders:Map[String,FileSet] = Map()
-  val fileNames=mutable.Map[String,String]();
-  var my_name:String=_
-  var my_parent:String=_
-  var my_full_path:String=_
 
-  def add(myPatern:String,myName:String,oPath:String,path:Array[String],start:Int): Unit ={
-    if(path==null || path.length-start<=0){
+  val subFolders: Map[String, FileSet] = Map()
+  val fileNames = mutable.Map[String, String]();
+  var my_name: String = _
+  var my_parent: String = _
+  var my_full_path: String = _
+
+  def add(myPatern: String, myName: String, oPath: String, path: Array[String], start: Int): Unit = {
+    if (path == null || path.length - start <= 0) {
       return;
     }
-    my_name=myName
-    my_parent=myPatern
-    my_full_path=if(my_parent=="") my_name else my_parent+"/"+my_name
+    my_name = myName
+    my_parent = myPatern
+    my_full_path = if (my_parent == "") my_name else my_parent + "/" + my_name
 
-    val len=path.length-start
-    val fName=path(start);
-    if(len>1){
+    val len = path.length - start
+    val fName = path(start);
+    if (len > 1) {
 
-      if(!subFolders.contains(fName)){
-        subFolders.update(fName,new FileSet);
+      if (!subFolders.contains(fName)) {
+        subFolders.update(fName, new FileSet);
       }
-      subFolders.get(fName).get.add(my_full_path,fName,oPath,path,start+1)
+      subFolders.get(fName).get.add(my_full_path, fName, oPath, path, start + 1)
     }
-    else{
-      fileNames.update(fName,oPath);
+    else {
+      fileNames.update(fName, oPath);
     }
 
   }
--- SynchronizeFile/src/main/scala-2.11/org/openfly/tools/scala/syschronzie/SynchronizeFolder.scala
+++ SynchronizeFile/src/main/scala-2.11/org/openfly/tools/scala/syschronzie/SynchronizeFolder.scala
@@ -13,81 +13,89 @@ import scala.collection.mutable
  */
 object SynchronizeFolder {
 
-  def collectCopyList(targetRootPath:String,tFolder: File, sFolder: File, copyList: mutable.MutableList[(Path, Path)], force: Boolean, ignoreFileNames: Set[String], ignoreFilePaths:Set[String]): Unit = {
-    val subs=List(tFolder.listFiles():_*)
-    subs.foreach(one=>{
-      val fileName=one.getName
-      val filePath=one.getPath
-      val relativePath=filePath.substring(targetRootPath.length+1)
-      if(fileName=="." || fileName==".."){
+  def collectCopyList(targetRootPath: String, tFolder: File, sFolder: File, copyList: mutable.MutableList[(Path, Path)], force: Boolean, ignoreFileNames: Set[String], ignoreFilePaths: Set[String]): Unit = {
+    val subs = List(tFolder.listFiles(): _*)
+    subs.foreach(one => {
+      val fileName = one.getName
+      val filePath = one.getPath
+      val relativePath = filePath.substring(targetRootPath.length + 1)
+      if (fileName == "." || fileName == "..") {
         //ignore
       }
-      else if(ignoreFileNames.contains(fileName)){
+      else if (ignoreFileNames.contains(fileName)) {
         //ignore
       }
-      else if(ignoreFilePaths.contains(relativePath)){
+      else if (ignoreFilePaths.contains(relativePath)) {
         //ignore
       }
       else {
-        val s=new File(sFolder,fileName)
-        if(s.isFile && one.isFile && (force || s.lastModified()> one.lastModified())){
-          copyList += new Tuple2(s.toPath,one.toPath);
+        val s = new File(sFolder, fileName)
+        if (s.isFile && one.isFile && (force || s.lastModified() > one.lastModified())) {
+          copyList += new Tuple2(s.toPath, one.toPath);
         }
-        else if(s.isDirectory && one.isDirectory){
-          collectCopyList(targetRootPath,one,s,copyList,force,ignoreFileNames,ignoreFilePaths)
+        else if (s.isDirectory && one.isDirectory) {
+          collectCopyList(targetRootPath, one, s, copyList, force, ignoreFileNames, ignoreFilePaths)
         }
       }
     })
 
   }
 
-  def main(argv:Array[String]): Unit ={
-    val p=new ParameterParser(argv)
+  def main(argv: Array[String]): Unit = {
+    val p = new ParameterParser(argv)
     p.add(Parameter("source-folder").addAliasKey("-s").setRequired(true).setFollowingValueSize(1))
     p.add(Parameter("target-folder").addAliasKey("-t").setRequired(true).setFollowingValueSize(1))
     p.add(Parameter("force-copy-all").addAliasKey("-f").setRequired(false).setFollowingValueSize(0));
     p.add(Parameter("ignore-file-name").addAliasKey("-in").setRequired(false).setFollowingValueSize(1));
     p.add(Parameter("ignore-file-path").addAliasKey("-ip").setRequired(false).setFollowingValueSize(1));
     p.add(Parameter("ignore-file-list-file").addAliasKey("-if").setRequired(false).setFollowingValueSize(1));
+    p.add(Parameter("dry-run").setRequired(false).setDesc("Only list the copy which need copy, do not do the copy action!"))
     p.add(Parameter("help").setRequired(false).setFollowingValueSize(0))
 
     p.parse();
 
-    if(p.isSet("help")){
-      println ("Usage:"+p.usage)
+    if (p.isSet("help")) {
+      println("Usage:" + p.usage)
       return
     }
 
-    val es=p.getErrorMessage()
+    val es = p.getErrorMessage()
 
-    if(es.size>0){
+    if (es.size > 0) {
       es.foreach(println(_))
       println("error, quite")
-      println ("Usage:"+p.usage)
+      println("Usage:" + p.usage)
       return
     }
-    val sFolder=new File(p.getFirstValue("source-folder").get)
+    val sFolder = new File(p.getFirstValue("source-folder").get)
 
-    require(sFolder.isDirectory,sFolder.getPath+" should be the dir")
+    require(sFolder.isDirectory, sFolder.getPath + " should be the dir")
 
-    val tFolder=new File(p.getFirstValue("target-folder").get)
-    require(tFolder.isDirectory,tFolder.getPath+" should be the dir")
-
-
-    val copyList=new mutable.MutableList[(Path,Path)];
-
-    val ignoreFileNames= Set[String]((p.getValues("ignore-file-name").getOrElse(Array[String]())):_*)
-
-    val ignoreFilePaths =Set[String]((p.getValues("ignore-file-path").getOrElse(Array[String]())):_*)
+    val tFolder = new File(p.getFirstValue("target-folder").get)
+    require(tFolder.isDirectory, tFolder.getPath + " should be the dir")
 
 
+    val copyList = new mutable.MutableList[(Path, Path)];
 
-    collectCopyList(tFolder.getPath,tFolder,sFolder,copyList,p.isSet("force-copy-all"),ignoreFileNames,ignoreFilePaths)
+    val ignoreFileNames = Set[String]((p.getValues("ignore-file-name").getOrElse(Array[String]())): _*)
+
+    val ignoreFilePaths = Set[String]((p.getValues("ignore-file-path").getOrElse(Array[String]())): _*)
 
 
-    copyList.foreach(one =>{
-      println(one._1+" -> "+one._2)
-      Files.copy(one._1,one._2,REPLACE_EXISTING)
+
+    collectCopyList(tFolder.getPath, tFolder, sFolder, copyList, p.isSet("force-copy-all"), ignoreFileNames, ignoreFilePaths)
+
+
+
+    copyList.foreach(one => {
+      println(one._1 + " -> " + one._2)
+      if (!p.isSet("dry-run")) {
+        Files.copy(one._1, one._2, REPLACE_EXISTING)
+      }
     })
+    if (p.isSet("dry-run")) {
+      println("dry-run ! ,No copy Action!")
+    }
+
   }
 }
